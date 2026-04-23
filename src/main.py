19e64caa5b5bd8ddbb89f0d4cc0512b0347cb249
Kayque Pereira <114672278+kaypes@users.noreply.github.com>
2026-04-23 14:33:14 -0300

    feat: orquestrar injecao de dependencias e loop principal
    finaliza o ponto de entrada da aplicação. Instancia os controladores de hardware e UI, injeta a fila de eventos compartilhada e inicia a Máquina de Estados dentro do event loop do uasyncio

--- src/main.py
+++ src/main.py
@@ -1,1 +1,24 @@
+import uasyncio
+
+from hal import FeedbackController
+from display_driver import DisplayDriver
+from fsm import FSM
+from async_queue import Queue
+from input_handler import ButtonMatrix
+
 print("Teste")
+
+
+async def main():
+    event_queue = Queue(maxsize=16)
+
+    feedback = FeedbackController()
+    display = DisplayDriver()
+    _ = ButtonMatrix(event_queue=event_queue)
+
+    fsm = FSM(event_queue=event_queue, feedback=feedback, display=display)
+
+    await fsm.run()
+
+
+uasyncio.run(main())
